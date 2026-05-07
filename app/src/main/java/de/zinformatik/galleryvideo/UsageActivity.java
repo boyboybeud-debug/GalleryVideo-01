59cefcf75cd12804ed9ad2ceb7bdf1ed21686a86
boyboybeud-debug <boyboybeud@gmail.com>
2026-05-07 08:04:08 +0800

    Create UsageActivity.java

---
+++ app/src/main/java/de/zinformatik/galleryvideo/UsageActivity.java
@@ -0,0 +1,13 @@
+package de.zinformatik.galleryvideo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UsageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usage);
+    }
+}
